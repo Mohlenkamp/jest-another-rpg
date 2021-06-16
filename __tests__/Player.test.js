1f3462490170a9557a0c277e4e572dd3685f3e6b
John Mohlenkamp <john@mohlenkamp.com>
2021-06-16 10:58:29 -0500

    finished 10.3.4

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -4,7 +4,7 @@ const Player = require('../lib/Player.js')
 const Potion = require('../lib/Potion.js')
 
 jest.mock('../lib/Potion.js')
-console.log(new Potion());
+//console.log(new Potion());
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -64,4 +64,31 @@ test('creates a player object', () => {
     player.reduceHealth(99999);
   
     expect(player.health).toBe(0);
+  });
+
+  test("gets player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+  
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+  });
+
+  test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    const oldCount = player.inventory.length;
+  
+    player.addPotion(new Potion());
+  
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+  });
+
+  test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+  
+    player.usePotion(1);
+  
+    expect(player.inventory.length).toBeLessThan(oldCount);
   });
